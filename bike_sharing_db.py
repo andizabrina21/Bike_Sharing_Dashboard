70e84be6c9c9ff20ce9cb2cc360aa6a7b0a296cd
andizabrina <159792981+andizabrina21@users.noreply.github.com>
2026-04-22 17:37:51 +0700

    Update bike_sharing_db.py

--- bike_sharing_db.py
+++ bike_sharing_db.py
@@ -20,7 +20,7 @@ def create_daily_sharing_df(df):
     return daily_sharing_df
 
 def create_peak_hour_df(df):
-    peak_hour_df = hour_df.groupby("hr")["cnt"].sum().idxmax()
+    peak_hour_df = df.groupby("hr")["cnt"].sum().idxmax()
     return peak_hour_df
 
 def create_total_sharing_bike_df(df):
