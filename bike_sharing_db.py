40bab68f56fccbbe8a4407f831d260926ef8406c
andizabrina <159792981+andizabrina21@users.noreply.github.com>
2026-04-23 20:19:17 +0700

    Update bike_sharing_db.py

--- bike_sharing_db.py
+++ bike_sharing_db.py
@@ -37,7 +37,7 @@ def create_byday_df(df):
         0: 'Sunday',1: 'Monday', 
         2: 'Tuesday', 3: 'Wednesday', 
         4: 'Thursday', 5: 'Friday', 
-        6: 'Sunday'}), ordered=True)
+        6: 'Saturday'}), ordered=True)
 
     return byday_df
 
